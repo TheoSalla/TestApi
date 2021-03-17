50ffd64ad2c83aaf70d19559559f8e1e821d1da5
Theodor <nosmhee@gmail.com>
2021-03-17 12:35:50 +0100

    Removed https redirection

--- src/SimepleAPI/Startup.cs
+++ src/SimepleAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace SimepleAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimepleAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
